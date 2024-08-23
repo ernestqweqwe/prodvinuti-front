bb2cb5d18e298a2aedb3c74425eae782a8b241d5
Ernestas Yushkiavichus <kenyrofa@gmail.com>
2024-08-23 11:07:58 +0300

    Урок 7. организация стилей, переключение тем

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import { Suspense } from 'react';
+import { Suspense, useState } from 'react';
 import { Link, Route, Routes } from 'react-router-dom';
-import './index.scss';
+import './styles/index.scss';
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
 import { MainPageAsync } from './pages/MainPage/MainPage.async';
 
+export enum Theme {
+  LIGHT = 'light',
+  DARK = 'dark',
+}
+
 const App = () => {
+  const [theme, setTheme] = useState<Theme>(Theme.LIGHT);
+
+  const toggleTheme = () => {
+    setTheme(theme === Theme.DARK ? Theme.LIGHT : Theme.DARK);
+  };
   return (
-    <div className="App">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>TOGGLE</button>
       <Link to={'/'}>Главная страница</Link>
       <Link to={'/about'}>О сайте</Link>
       <Suspense fallback={<div>Загрузка...</div>}>
