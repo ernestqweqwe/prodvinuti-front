33bf14307c937ee8f4a240e418c5a7c65c9c27e5
Ernestas Yushkiavichus <kenyrofa@gmail.com>
2024-08-24 09:12:18 +0300

    Урок 10(применение FSD методологии)

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -1,7 +1,12 @@
 import { ResolveOptions } from 'webpack';
+import { BuildOptions } from './types/config';
 
-export function buildResolvers(): ResolveOptions {
+export function buildResolvers(options: BuildOptions): ResolveOptions {
   return {
     extensions: ['.tsx', '.ts', '.js'], // позваляет для файлов с данными расширениями не указывать полный путь при импорте (напр import {someFn} from './test') без .js
+    preferAbsolute: true,
+    modules: [options.paths.src, 'node_modules'],
+    mainFiles: ['index'],
+    alias: {},
   };
 }
