124f7ae93c0bc96766a0c6eac92d92dcc66a49b4
Ernestas Yushkiavichus <kenyrofa@gmail.com>
2024-09-05 09:54:20 +0300

    21 Несуществующие маршруты. Лоадер для загрузки страниц

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,11 @@ module.exports = {
         es2021: true,
         jest: true,
     },
-    extends: ['plugin:react/recommended', 'airbnb', 'plugin:i18next/recommended'],
+    extends: [
+        'plugin:react/recommended',
+        'airbnb',
+        'plugin:i18next/recommended',
+    ],
     parser: '@typescript-eslint/parser',
     parserOptions: {
         ecmaFeatures: {
@@ -18,7 +22,10 @@ module.exports = {
         'react/jsx-indent': [2, 4],
         'react/jsx-indent-props': [2, 4],
         indent: [2, 4],
-        'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.tsx'] }],
+        'react/jsx-filename-extension': [
+            2,
+            { extensions: ['.js', '.jsx', '.tsx'] },
+        ],
         'import/no-unresolved': 'off',
         'import/prefer-default-export': 'off',
         'no-unused-vars': 'warn',
@@ -35,6 +42,7 @@ module.exports = {
         'object-curly-newline': 'off',
         'implicit-arrow-linebreak': 'off',
         'i18next/no-literal-string': ['error'],
+        'max-len': ['error', { ignoreComments: true }],
     },
     globals: {
         __IS_DEV__: true,
--- src/shared/lib/classNames/classNames.test.ts
+++ src/shared/lib/classNames/classNames.test.ts
@@ -7,27 +7,38 @@ describe('classNames', () => {
 
     test('With aditional class', () => {
         const expected = 'someClass class1 class2';
-        expect(classNames('someClass', {}, ['class1', 'class2'])).toBe(expected);
+        expect(classNames('someClass', {}, ['class1', 'class2'])).toBe(
+            expected,
+        );
     });
 
     test('With mods', () => {
         const expected = 'someClass class1 class2 hovered scrollbale';
         expect(
-            classNames('someClass', { hovered: true, scrollbale: true }, ['class1', 'class2']),
+            classNames('someClass', { hovered: true, scrollbale: true }, [
+                'class1',
+                'class2',
+            ]),
         ).toBe(expected);
     });
 
     test('With mods false', () => {
         const expected = 'someClass class1 class2 hovered';
         expect(
-            classNames('someClass', { hovered: true, scrollbale: false }, ['class1', 'class2']),
+            classNames('someClass', { hovered: true, scrollbale: false }, [
+                'class1',
+                'class2',
+            ]),
         ).toBe(expected);
     });
 
     test('With mods undefined', () => {
         const expected = 'someClass class1 class2 hovered';
         expect(
-            classNames('someClass', { hovered: true, scrollbale: undefined }, ['class1', 'class2']),
+            classNames('someClass', { hovered: true, scrollbale: undefined }, [
+                'class1',
+                'class2',
+            ]),
         ).toBe(expected);
     });
 });
